b766bffa35e67a0ab29e5eb3c5099d0a622a67dc
stellarDev0503 <paulguru.sw@gmail.com>
2019-03-24 13:20:28 -0500

    Change number of trials from 100,000 to 10,000

--- ch08-conditional-logic/7-simulate-events-and-calculate-probabilities.py
+++ ch08-conditional-logic/7-simulate-events-and-calculate-probabilities.py
@@ -6,20 +6,15 @@ from random import randint
 
 
 # Exercise 1
-# Write a function that simulatee the roll of a die.
-def roll():
-    """Return random integer between 1 and 6"""
-    return randint(1, 6)
+# Simulate the toss of a die
+print(randint(1, 6))
 
 
 # Exercise 2
-# Simulate 10,000 rolls of a die and display the average number rolled.
+# Simulate 10,000 throws of dice and display the average result
 trials = 10000
 total = 0
-
 for trial in range(0, trials):
-    total = total + roll()
-
-avg_roll = total / trials
-
-print(f"The average result of {trials} throws is {avg_roll}")
+    total += randint(1, 6)
+avg_result = total / trials
+print(f"The average result of {trials} throws was {avg_result}")
